d478de6c6270e82987f73e53940aa54473df14b9
nrvarun <nr.varun79@gmail.com>
2017-02-11 18:20:31 +0530

    server.js file modified

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
-'use strict';
+var path 	=	require('path');
+var express = 	require('express');
+var app 	= 	express();
 
-let express = require('express');
-let path = require('path');
-let app = express();
-let isProduction = process.env.NODE_ENV === 'production';
-let port = isProduction ? process.env.PORT : 3000;
-let publicPath = path.resolve(__dirname, 'dist');
- // We point to our static assets
-app.use(express.static(publicPath));
- // And run the server
-app.listen(port, function () {
-  console.log('Server running on port ' + port);
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+// set the view engine to ejs
+app.set('view engine', 'jade');
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(path.join(__dirname + '/dist')));
+
+// set the home page route
+app.get('/', function(req, res) {
+
+	// ejs render automatically looks in the views folder
+	res.render('index');
+});
+
+app.listen(port, function() {
+	console.log('Our app is running on http://localhost:' + port);
 });
